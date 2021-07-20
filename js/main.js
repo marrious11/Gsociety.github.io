014f1881523ab06f19ab3da9c0270be4022c091a
Soh Marrious <sohmarrious@gmail.com>
2021-07-20 07:45:36 +0100

    Add js and more

---
+++ js/main.js
@@ -0,0 +1,30 @@
+jQuerry(document).ready(function(){
+
+    "use strick";
+     $('#slider-caruosel').caroufredsel({
+         resposive:true,
+         width:'100%',
+         circular:true,
+         scroll:{
+             items:1,
+             duration:500,
+             pauseOnHover:true
+         },
+         auto:true,
+         items:{
+             visible:{
+                 min:1,
+                 max:1
+             },
+             height:"variable"
+         },
+         pagination:{
+             container:".sliderpager",
+             pageAnchorBuilder:false
+         }
+     });
+
+
+
+
+});
